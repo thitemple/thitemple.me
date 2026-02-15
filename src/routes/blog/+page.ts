ad4e3de9c601afc3955737b3b31bb3e33b0283a0
Thiago Temple <thitemple@icloud.com>
2026-02-15 06:47:28 -0500

    Enable prerender and direct posts

--- src/routes/blog/+page.ts
+++ src/routes/blog/+page.ts
@@ -1,7 +1,5 @@
 import { redirect } from "@sveltejs/kit";
 
-export async function load({ url }) {
-	const page = url.searchParams.get("page");
-	const target = page ? `/writing?page=${page}` : "/writing";
-	redirect(301, target);
+export async function load() {
+	redirect(301, "/writing");
 }
